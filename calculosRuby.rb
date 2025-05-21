979e7f6d9d72ca0386986b29aa34c370a32330ff
victorreno2009 <victorreno2009@gmail.com>
2025-05-21 14:36:14 -0300

    Calculo Ruby

---
+++ calculosRuby.rb
@@ -0,0 +1,54 @@
+puts("[1] para soma, [2] para subtração, [3] para multiplicação. [4] para divisão")
+print "Escolha uma operação: "
+escolha = gets.chomp.to_i
+
+if escolha == 1
+    puts "SOMA"
+    print "Digite um número: "
+    num1 = gets.chomp.to_i
+    print "Digite outro número: "
+    num2 = gets.chomp.to_i
+
+    soma = num1 + num2
+
+    puts "A soma é: #{soma}"
+end
+
+if escolha == 2
+    puts "SUBTRAÇÃO"
+    print "Digite um número: "
+    num1 = gets.chomp.to_i
+    print "Digite outro número: "
+    num2 = gets.chomp.to_i
+
+    soma = num1 - num2
+
+    puts "A subtração é: #{soma}"
+end
+
+if escolha == 3
+    puts "MULTIPLICAÇÃO"
+    print "Digite um número: "
+    num1 = gets.chomp.to_i
+    print "Digite outro número: "
+    num2 = gets.chomp.to_i
+
+    soma = num1 * num2
+
+    puts "A multiplicação é: #{soma}"
+end
+
+if escolha == 4
+     puts "DIVISÃO"
+    print "Digite um número: "
+    num1 = gets.chomp.to_i
+    print "Digite outro número: "
+    num2 = gets.chomp.to_i
+
+    soma = num1 / num2
+
+    puts "A divisão é: #{soma}"
+end
+
+
+
